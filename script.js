e451344bd377dc2b1b77616982a7d53c28d01206
emilymorosoff <emilymorosoff@icloud.com>
2023-11-05 02:19:03 -0800

    Implemented the functionality to save events to local storage (JavaScript).

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
 $(document).ready(function() {
   var today = moment().format('dddd, MMMM Do');
   $('#currentDay').text(today);
-});
 
-function updateTimeBlockColors() {
-  var currentHour = moment().hour(); 
+  function updateTimeBlockColors() {
+      var currentHour = moment().hour(); 
+      $('.time-block').each(function() {
+          var blockHour = parseInt($(this).data('hour'));
+          if (blockHour < currentHour) {
+              $(this).addClass('past').removeClass('present future');
+          } else if (blockHour === currentHour) {
+              $(this).addClass('present').removeClass('past future');
+          } else {
+              $(this).addClass('future').removeClass('past present');
+          }
+      });
+  }
 
-  $('.time-block').each(function() {
-      var blockHour = parseInt($(this).data('hour'));
-
-      if (blockHour < currentHour) {
-          $(this).addClass('past');
-      } else if (blockHour === currentHour) {
-          $(this).addClass('present');
-      } else {
-          $(this).addClass('future');
-      }
-  });
-}
-
-$(document).ready(function() {
   updateTimeBlockColors();
-});
 
-function saveEvent(hour, eventData) {
-  localStorage.setItem('event-' + hour, eventData);
-}
+  function saveEvent(hour, eventData) {
+      localStorage.setItem('event-' + hour, eventData);
+  }
 
-$(document).on('click', '.save-btn', function() {
-  var hour = $(this).closest('.time-block').data('hour');
-  var eventData = $(this).siblings('.event-input').val();
-  saveEvent(hour, eventData);
+  function loadSavedEvents() {
+      $('.time-block').each(function() {
+          var hour = $(this).data('hour');
+          var savedEvent = localStorage.getItem('event-' + hour);
+          if (savedEvent) {
+              $(this).find('.event-input').val(savedEvent);
+          }
+      });
+  }
+
+  loadSavedEvents();
+
+  $(document).on('click', '.save-btn', function() {
+      var hour = $(this).closest('.time-block').data('hour');
+      var eventData = $(this).siblings('.event-input').val();
+      saveEvent(hour, eventData);
+  });
 });
